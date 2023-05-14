c79be9e7dc09962a2dac7cb1a9b6dd557a67de70
hboff <oskar@baucampus.de>
2023-05-14 22:36:28 +0200

    recruting + visum

--- recruiting.php
+++ recruiting.php
@@ -49,6 +49,51 @@ Verfügung, um Sie bei der Einstellung von Mitarbeitern zu unterstützen.</p>
         </div>
     </div>
     </div>
+
+
+
+
+        <div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-lg-10">
+            <div class="px-5 py-5">
+                <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                    <h1 class="display-5 mb-5">Personalvermittlung</h1>
+                </div>
+
+                <div class="col-md-8 mx-auto">
+                <p>Die Personalvermittlung oder das Recruiting ist ein wichtiger Aspekt für Unternehmen, die in den Vereinigten Arabischen
+                Emiraten tätig sind oder sich in der Region niederlassen möchten. Der Wettbewerb um die besten Talente ist hoch und es
+                kann schwierig sein, qualifizierte Mitarbeiter zu finden, die den Anforderungen des Unternehmens entsprechen. Ein
+                professioneller Personalvermittlungsdienst kann dabei helfen, die richtigen Mitarbeiter zu finden und das Unternehmen
+                bei der Einstellung und der Integration von Mitarbeitern zu unterstützen.</p>
+
+                <p>Pantheon Elite bietet einen umfassenden Personalvermittlungsdienst an, der sich auf die Vermittlung von qualifizierten
+                Fachkräften in verschiedenen Branchen spezialisiert hat. Unser Team von erfahrenen Personalvermittlern arbeitet eng mit
+                unseren Kunden zusammen, um deren spezifischen Anforderungen und Bedürfnisse zu verstehen und geeignete Kandidaten zu
+                identifizieren, die den Anforderungen des Unternehmens entsprechen.</p>
+
+                <p>Wir verstehen, dass jedes Unternehmen unterschiedliche Anforderungen und Bedürfnisse hat, wenn es um die Einstellung
+                neuer Mitarbeiter geht. Deshalb bieten wir maßgeschneiderte Lösungen an, die auf die Bedürfnisse und Anforderungen
+                unserer Kunden zugeschnitten sind. Wir haben Zugang zu einem umfangreichen Netzwerk von qualifizierten Fachkräften, das
+                uns ermöglicht, schnell und effizient Kandidaten zu identifizieren, die den Anforderungen unserer Kunden entsprechen.</p>
+
+                <p>Unser Personalvermittlungsdienst umfasst auch die Unterstützung bei der Beantragung von Arbeitsvisa und die Integration
+                von Mitarbeitern in das Unternehmen. Wir unterstützen unsere Kunden bei der Durchführung von Hintergrundüberprüfungen,
+                der Verhandlung von Gehältern und Vergünstigungen sowie bei der Integration von Mitarbeitern in das Unternehmen. Wir
+                sind bestrebt, unseren Kunden eine umfassende Unterstützung bei der Einstellung von Mitarbeitern zu bieten, um
+                sicherzustellen, dass die Einstellung reibungslos verläuft und die Mitarbeiter produktiv und zufrieden sind.</p>
+
+                <p>Insgesamt bietet Pantheon Elite einen professionellen und effektiven Personalvermittlungsdienst an, der auf die
+                Bedürfnisse und Anforderungen unserer Kunden zugeschnitten ist. Wir sind bestrebt, unseren Kunden qualifizierte
+                Fachkräfte zu vermitteln, die den Anforderungen des Unternehmens entsprechen und zu einer erfolgreichen
+                Geschäftsentwicklung beitragen können. Unser erfahrenes Team von Personalvermittlern steht Ihnen jederzeit zur
+                Verfügung, um Sie bei der Einstellung von Mitarbeitern zu unterstützen.</p>
+                </div>
+                </div>
+        </div>
+    </div>
+</div>
     <!-- Team End -->
 <?php include 'kontakt-incl.php';?>
 
---
+++ visum.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+
+<body>
+    <?php include 'navbar.php';?>
+
+    <!-- Carousel Start -->
+    <div class="fullscreen position-relative">
+        <img src="img/kontoeroeffnung.jpg" alt="Dubai"
+            style="float:left;width:100vw;height:70vh;object-fit:cover;   object-position: center">
+    </div>
+    <!-- Carousel End -->
+
+
+
+    <!-- Team Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h1 class="display-5 mb-5">Visum</h1>
+                <p class="fs-5 fw-medium text-primary">Visum</p>
+
+            </div>
+           <p class="fs-5 fw-medium ">Ganz gleich, ob Sie eine Arbeits-, Investitions-, Familienvisum oder eine Aufenthaltsbewilligung benötigen sind wir für
+        Sie da. Wir informieren Sie über die Bedingungen für die Erlangung aller Kategorien von Visa und
+        Aufenthaltsgenehmigungen. Wir begleiten Sie von der Antragstellung an bis zur erfolgreichen Erlangung des gewünschten
+        Ziels.</p>
+            </div>
+        </div>
+    </div>
+    <!-- Team End -->
+
+
+    <?php include 'kontakt-incl.php';?>
+
+    <?php include 'footer.php';?>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
