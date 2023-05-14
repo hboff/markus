2ff450191c679797d403702d8e9a405627993fce
hboff <oskar@baucampus.de>
2023-05-14 23:09:15 +0200

    fix dienstleistungen

--- dienstleistungen.php
+++ dienstleistungen.php
@@ -1,5 +1,12 @@
 <?php include 'header.php';?>
-
+<style>
+        .service-item .service-text {
+            flex-grow: 1;
+        }
+        .fullscreen {
+            height: 100vh;
+        }
+        </style>
 <body>
     <?php include 'navbar.php';?>
 
@@ -16,8 +23,7 @@
 <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <h1 class="display-5 mb-5">Ihr Partner in Dubai</h1>
-            <p class="fs-5 fw-medium text-primary">Unsere Dienstleistungen</p>
+            <h1 class="display-5 mb-5">Unsere Dienstleistungen</h1>
 
         </div>
         <div class="row g-4">
