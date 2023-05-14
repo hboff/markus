39ba60c6915b4401eeaec8df8d682cfbd1d4e2a1
hboff <oskar@baucampus.de>
2023-05-14 23:07:30 +0200

    dienstleistungen

---
+++ dienstleistungen.php
@@ -0,0 +1,151 @@
+<?php include 'header.php';?>
+
+<body>
+    <?php include 'navbar.php';?>
+
+    <!-- Carousel Start -->
+    <div class="fullscreen position-relative">
+        <img src="img/kontoeroeffnung.jpg" alt="Dubai"
+            style="float:left;width:100vw;height:70vh;object-fit:cover;   object-position: center">
+    </div>
+    <!-- Carousel End -->
+
+
+
+    <!-- Team Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+            <h1 class="display-5 mb-5">Ihr Partner in Dubai</h1>
+            <p class="fs-5 fw-medium text-primary">Unsere Dienstleistungen</p>
+
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="service-item position-relative h-100 d-flex flex-column">
+                    <div class="service-text rounded p-5">
+                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="img/icon/icon-5.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Aufenthaltsgenehmigung / Visum</h4>
+                            <p class="mb-0">Wir unterstützen Sie bei der Beantragung des passenden Visums für Dubai, um
+                                Ihnen einen reibungslosen und schnellen
+                                Visumerhalt zu ermöglichen.</p>
+                    </div>
+                    <div class="service-btn rounded-0 rounded-bottom">
+                        <a class="text-primary fw-medium" href="/visum.php">Read More<i
+                                class="bi bi-chevron-double-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item position-relative h-100 d-flex flex-column">
+                  <div class="service-text rounded p-5">
+                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="img/icon/icon-6.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Unternehmensgründung</h4>
+                            <p class="mb-0">Unser Team hilft Ihnen bei der Gründung Ihrer Firma in Dubai, indem es Sie
+                                durch den gesamten Prozess begleitet, von der
+                                Auswahl der richtigen Rechtsform bis hin zur Registrierung und Geschäftsplanung.</p>
+                    </div>
+                    <div class="service-btn rounded-0 rounded-bottom">
+                        <a class="text-primary fw-medium" href="/unternehmensgruendung.php">Read More<i
+                                class="bi bi-chevron-double-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item position-relative h-100 d-flex flex-column">
+                    <div class="service-text rounded p-5">
+                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="img/icon/icon-7.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Kontoeröffung</h4>
+                            <p class="mb-0">Wir bieten maßgeschneiderte Lösungen für die Eröffnung von Bankkonten in
+                                Dubai und begleiten Sie durch den gesamten
+                                Prozess, um Ihren individuellen Bedürfnissen gerecht zu werden.</p>
+                    </div>
+                    <div class="service-btn rounded-0 rounded-bottom">
+                        <a class="text-primary fw-medium" href="/kontoeroeffnung.php">Read More<i
+                                class="bi bi-chevron-double-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item position-relative h-100 d-flex flex-column">
+                    <div class="service-text rounded p-5">
+                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="img/icon/icon-8.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Immobilienkauf</h4>
+                            <p class="mb-0">Unsere erfahrenen Immobilienexperten unterstützen Sie bei der Suche, Auswahl
+                                und dem Kauf der idealen Immobilie in
+                                Dubai, die Ihren persönlichen Anforderungen entspricht.</p>
+                    </div>
+                    <div class="service-btn rounded-0 rounded-bottom">
+                        <a class="text-primary fw-medium" href="/immobilienkauf.php">Read More<i
+                                class="bi bi-chevron-double-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item position-relative h-100 d-flex flex-column">
+                    <div class="service-text rounded p-5">
+                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="img/icon/icon-9.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Personalvermittlung</h4>
+                            <p class="mb-0">Unser Expertenteam kann Sie bei der Suche, Auswahl und Einstellung von
+                                qualifiziertem Personal für Ihr Unternehmen in
+                                Dubai unterstützen, um den richtigen Talentpool für Ihr Unternehmen aufzubauen.</p>
+                    </div>
+                    <div class="service-btn rounded-0 rounded-bottom">
+                        <a class="text-primary fw-medium" href="/recruiting.php">Read More<i
+                                class="bi bi-chevron-double-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item position-relative h-100 d-flex flex-column">
+                    <div class="service-text rounded p-5">
+                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+                            <img class="img-fluid" src="img/icon/icon-10.png" alt="Icon">
+                        </div>
+                        <h5 class="mb-3">Umzugsservice</h4>
+                            <p class="mb-0">Wir unterstützen Sie bei der Organisation Ihres Umzugs nach Dubai,
+                                einschließlich der Suche nach geeigneter Unterkunft,
+                                der Organisation von Transport und Logistik sowie der Anmeldung bei lokalen Behörden und
+                                Einrichtungen.</p>
+                    </div>
+                    <div class="service-btn rounded-0 rounded-bottom">
+                        <a class="text-primary fw-medium" href="/logistik.php">Read More<i
+                                class="bi bi-chevron-double-right ms-2"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <!-- Team End -->
+
+
+    <?php include 'kontakt-incl.php';?>
+
+    <?php include 'footer.php';?>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
