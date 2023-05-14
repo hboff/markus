40650e8ac7048221a3a2cb019d0053b319d00587
hboff <oskar@baucampus.de>
2023-05-14 22:02:28 +0200

    Umstellung php

---
+++ recruiting.php
@@ -0,0 +1,81 @@
+<?php include 'header.php';?>
+
+<body>
+<?php include 'navbar.php';?>
+
+
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container text-center py-5">
+            <h1 class="display-2 text-white mb-4 animated slideInDown">Visum</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item text-primary" aria-current="page">About</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+
+
+    <!-- Team Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h1 class="display-5 mb-5">Personalvermittlung</h1>
+                <p class="fs-5 fw-medium text-primary">Pantheon-Elite</p>
+
+            </div>
+<p class="fs-5 fw-medium ">Die Personalvermittlung oder das Recruiting ist ein wichtiger Aspekt für Unternehmen, die in den Vereinigten Arabischen
+Emiraten tätig sind oder sich in der Region niederlassen möchten. Der Wettbewerb um die besten Talente ist hoch und es
+kann schwierig sein, qualifizierte Mitarbeiter zu finden, die den Anforderungen des Unternehmens entsprechen. Ein
+professioneller Personalvermittlungsdienst kann dabei helfen, die richtigen Mitarbeiter zu finden und das Unternehmen
+bei der Einstellung und der Integration von Mitarbeitern zu unterstützen.</p>
+
+<p class="fs-5 fw-medium ">Pantheon Elite bietet einen umfassenden Personalvermittlungsdienst an, der sich auf die Vermittlung von qualifizierten
+Fachkräften in verschiedenen Branchen spezialisiert hat. Unser Team von erfahrenen Personalvermittlern arbeitet eng mit
+unseren Kunden zusammen, um deren spezifischen Anforderungen und Bedürfnisse zu verstehen und geeignete Kandidaten zu
+identifizieren, die den Anforderungen des Unternehmens entsprechen.</p>
+
+<p class="fs-5 fw-medium ">Wir verstehen, dass jedes Unternehmen unterschiedliche Anforderungen und Bedürfnisse hat, wenn es um die Einstellung
+neuer Mitarbeiter geht. Deshalb bieten wir maßgeschneiderte Lösungen an, die auf die Bedürfnisse und Anforderungen
+unserer Kunden zugeschnitten sind. Wir haben Zugang zu einem umfangreichen Netzwerk von qualifizierten Fachkräften, das
+uns ermöglicht, schnell und effizient Kandidaten zu identifizieren, die den Anforderungen unserer Kunden entsprechen.</p>
+
+<p class="fs-5 fw-medium ">Unser Personalvermittlungsdienst umfasst auch die Unterstützung bei der Beantragung von Arbeitsvisa und die Integration
+von Mitarbeitern in das Unternehmen. Wir unterstützen unsere Kunden bei der Durchführung von Hintergrundüberprüfungen,
+der Verhandlung von Gehältern und Vergünstigungen sowie bei der Integration von Mitarbeitern in das Unternehmen. Wir
+sind bestrebt, unseren Kunden eine umfassende Unterstützung bei der Einstellung von Mitarbeitern zu bieten, um
+sicherzustellen, dass die Einstellung reibungslos verläuft und die Mitarbeiter produktiv und zufrieden sind.</p>
+
+<p class="fs-5 fw-medium ">Insgesamt bietet Pantheon Elite einen professionellen und effektiven Personalvermittlungsdienst an, der auf die
+Bedürfnisse und Anforderungen unserer Kunden zugeschnitten ist. Wir sind bestrebt, unseren Kunden qualifizierte
+Fachkräfte zu vermitteln, die den Anforderungen des Unternehmens entsprechen und zu einer erfolgreichen
+Geschäftsentwicklung beitragen können. Unser erfahrenes Team von Personalvermittlern steht Ihnen jederzeit zur
+Verfügung, um Sie bei der Einstellung von Mitarbeitern zu unterstützen.</p>
+        </div>
+    </div>
+    </div>
+    <!-- Team End -->
+<?php include 'kontakt-incl.php';?>
+
+<?php include 'footer.php';?>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
