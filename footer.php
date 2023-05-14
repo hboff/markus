b1798210530cded73f825f73ae95b9de2697c983
hboff <oskar@baucampus.de>
2023-05-14 21:43:55 +0200

    test

---
+++ footer.php
@@ -0,0 +1,46 @@
+    <!-- Footer Start -->
+    <div class="container-fluid footer  py-5 wow fadeIn" data-wow-delay="0.1s" style="background-color: #002440;">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Unser Büro</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Meydan, Dubai</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+971 50 2564749</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@pantheon-elite.ae</p>
+                    <div class="d-flex pt-3">
+                    <a class="btn btn-square btn-light rounded-circle me-2" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0"><i class="fab fa-whatsapp"></i></a>
+                    <a class="btn btn-square btn-light rounded-circle me-2" href="ttps://t.me/Baucampus_bot"><i class="fab fa-telegram"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Links</h4>
+                    <a class="btn btn-link" href="">Über Uns</a>
+                    <a class="btn btn-link" href="">Kontakt</a>
+                    <a class="btn btn-link" href="">Unsere Dienstleistungen</a>
+                    <a class="btn btn-link" href="">Datenschutzerklärung</a>
+                    <a class="btn btn-link" href="">Impressum</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Öffnungszeiten</h4>
+                    <p class="mb-1">Montag - Freitag</p>
+                    <h6 class="text-light">09:00 - 17:00</h6>
+                    <p class="mb-1">Samstag</p>
+                    <h6 class="text-light">09:00 - 12:00</h6>
+                    <p class="mb-1">Sonntag</p>
+                    <h6 class="text-light">09:00 - 12:00</h6>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Social Media</h4>
+                    <div class="d-flex pt-3">
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-square btn-light rounded-circle me-2" href=""><i
+                                class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
---
+++ navbar.php
@@ -0,0 +1,47 @@
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0" style=" backdrop-filter: blur(25px) invert(.2);  background-color: rgba(255, 255, 255, .15);">
+        <div class="container">
+            <a class="navbar-brand" href="#!"><span style="color: #5e9693;">Pantheon</span><span
+                    style="color: #fff;">Elite</span></a>
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="dienstleistungen">Dienstleistungen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="team.html">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Kontakt</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav d-flex flex-row">
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0">
+                            <i class="fab fa-whatsapp"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://t.me/Baucampus_bot">
+                            <i class="fab fa-telegram"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0">
+                            EN 
+                        </a>
+                    </li><li class="nav-item me-3 me-lg-0">
+                        <a class="nav-link" href="https://api.whatsapp.com/send/?phone=971561642909&text&type=phone_number&app_absent=0">
+                            RU
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<!-- Navbar -->
