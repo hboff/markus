a7c7e5b05cd8cceb0d23efa8951c4f7d14d859fb
hboff <oskar@baucampus.de>
2023-05-14 23:02:50 +0200

    fix kontakt-incl

--- index.php
+++ index.php
@@ -449,25 +449,7 @@
         </div>
     </div>
     <!-- Team End -->
-    <!-- Kontakt2 Start -->
 
-<section class="page-section bg-light">
-        <div class="container-xxl py-5">    
-            <div class="container">
-                <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                    <h1 class="display-5 mb-5">Kontakformular</h1>
-                </div>
-            </div>
-        <div class="calendly-inline-widget"
-                 data-url="https://calendly.com/oskarkelohse/erstbesprechung-immobilie-dubai?hide_gdpr_banner=1"
-                 style="min-width:320px;height:630px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async=""></script>
-            </div>
-
-</section>
-    <!-- Kontakt2 End -->
-
-    <!-- Footer Start -->
 <?php include 'kontakt-incl.php';?>
 
 <?php include 'footer.php';?>
