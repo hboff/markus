1b537968ac846b044e542891b57fc8fdd64f88a0
hboff <oskar@baucampus.de>
2023-05-15 13:52:26 +0200

    fix

--- ru/dienstleistungen.php
+++ ru/dienstleistungen.php
@@ -12,31 +12,31 @@
 
     <!-- Carousel Start -->
     <div class="fullscreen position-relative">
-        <img src="img/kontoeroeffnung.jpg" alt="Dubai"
+        <img src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/kontoeroeffnung.jpg" alt="Dubai"
             style="float:left;width:100vw;height:70vh;object-fit:cover;   object-position: center">
     </div>
     <!-- Carousel End -->
 
     <!-- Team Start -->
-<div class="container-xxl">
+<div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <h1 class="display-5 mb-5">Unsere Dienstleistungen</h1>
+            <h1 class="display-5 mb-5">Ваш партнер в Дубае</h1>
+            <p class="fs-5 fw-medium text-primary">Наши услуги</p>
+
         </div>
         <div class="row g-4">
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
             <div class="service-item position-relative h-100 d-flex flex-column">
                     <div class="service-text rounded p-5">
                         <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
-                            <img class="img-fluid" src="img/icon/icon-5.png" alt="Icon">
+                            <img class="img-fluid" src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/icon/icon-5.png" alt="Icon">
                         </div>
-                        <h5 class="mb-3">Aufenthaltsgenehmigung / Visum</h4>
-                            <p class="mb-0">Wir unterstützen Sie bei der Beantragung des passenden Visums für Dubai, um
-                                Ihnen einen reibungslosen und schnellen
-                                Visumerhalt zu ermöglichen.</p>
+                        <h5 class="mb-3">Разрешение на проживание / Виза</h4>
+                            <p class="mb-0">Мы поможем вам подать заявление на подходящую визу в Дубае, чтобы обеспечить быстрое и беспроблемное получение визы.</p>
                     </div>
                     <div class="service-btn rounded-0 rounded-bottom">
-                        <a class="text-primary fw-medium" href="/visum.php">Read More<i
+                        <a class="text-primary fw-medium" href="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>//visum.php">Подробнее<i
                                 class="bi bi-chevron-double-right ms-2"></i></a>
                     </div>
                 </div>
@@ -45,15 +45,13 @@
                 <div class="service-item position-relative h-100 d-flex flex-column">
                   <div class="service-text rounded p-5">
                         <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
-                            <img class="img-fluid" src="img/icon/icon-6.png" alt="Icon">
+                            <img class="img-fluid" src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/icon/icon-6.png" alt="Icon">
                         </div>
-                        <h5 class="mb-3">Unternehmensgründung</h4>
-                            <p class="mb-0">Unser Team hilft Ihnen bei der Gründung Ihrer Firma in Dubai, indem es Sie
-                                durch den gesamten Prozess begleitet, von der
-                                Auswahl der richtigen Rechtsform bis hin zur Registrierung und Geschäftsplanung.</p>
+                        <h5 class="mb-3">Основание компании</h4>
+                            <p class="mb-0">Наша команда поможет вам создать вашу компанию в Дубае, сопровождая вас на всем пути от выбора правильной формы собственности до регистрации и планирования бизнеса.</p>
                     </div>
                     <div class="service-btn rounded-0 rounded-bottom">
-                        <a class="text-primary fw-medium" href="/unternehmensgruendung.php">Read More<i
+                        <a class="text-primary fw-medium" href="/unternehmensgruendung.php">Подробнее<i
                                 class="bi bi-chevron-double-right ms-2"></i></a>
                     </div>
                 </div>
@@ -62,15 +60,13 @@
                 <div class="service-item position-relative h-100 d-flex flex-column">
                     <div class="service-text rounded p-5">
                         <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
-                            <img class="img-fluid" src="img/icon/icon-7.png" alt="Icon">
+                            <img class="fluid" src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/icon/icon-7.png" alt="Icon">
                         </div>
-                        <h5 class="mb-3">Kontoeröffung</h4>
-                            <p class="mb-0">Wir bieten maßgeschneiderte Lösungen für die Eröffnung von Bankkonten in
-                                Dubai und begleiten Sie durch den gesamten
-                                Prozess, um Ihren individuellen Bedürfnissen gerecht zu werden.</p>
+                        <h5 class="mb-3">Открытие счета</h5>
+                            <p class="mb-0">Мы предлагаем индивидуальные решения для открытия банковских счетов в Дубае и сопровождаем вас на всем пути, чтобы удовлетворить ваши индивидуальные потребности.</p>
                     </div>
                     <div class="service-btn rounded-0 rounded-bottom">
-                        <a class="text-primary fw-medium" href="/kontoeroeffnung.php">Read More<i
+                        <a class="text-primary fw-medium" href="/kontoeroeffnung.php">Подробнее<i
                                 class="bi bi-chevron-double-right ms-2"></i></a>
                     </div>
                 </div>
@@ -79,15 +75,13 @@
                 <div class="service-item position-relative h-100 d-flex flex-column">
                     <div class="service-text rounded p-5">
                         <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
-                            <img class="img-fluid" src="img/icon/icon-8.png" alt="Icon">
+                            <img class="img-fluid" src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/icon/icon-8.png" alt="Icon">
                         </div>
-                        <h5 class="mb-3">Immobilienkauf</h4>
-                            <p class="mb-0">Unsere erfahrenen Immobilienexperten unterstützen Sie bei der Suche, Auswahl
-                                und dem Kauf der idealen Immobilie in
-                                Dubai, die Ihren persönlichen Anforderungen entspricht.</p>
+                        <h5 class="mb-3">Покупка недвижимости</h5>
+                            <p class="mb-0">Наши опытные специалисты по недвижимости помогут вам в поиске, выборе и покупке идеальной недвижимости в Дубае, которая соответствует вашим личным требованиям.</p>
                     </div>
                     <div class="service-btn rounded-0 rounded-bottom">
-                        <a class="text-primary fw-medium" href="/immobilienkauf.php">Read More<i
+                        <a class="text-primary fw-medium" href="/immobilienkauf.php">Подробнее<i
                                 class="bi bi-chevron-double-right ms-2"></i></a>
                     </div>
                 </div>
@@ -96,16 +90,13 @@
                 <div class="service-item position-relative h-100 d-flex flex-column">
                     <div class="service-text rounded p-5">
                         <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
-                            <img class="img-fluid" src="img/icon/icon-9.png" alt="Icon">
+                            <img class="img-fluid" src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/icon/icon-9.png" alt="Icon">
                         </div>
-                        <h5 class="mb-3">Personalvermittlung</h4>
-                            <p class="mb-0">Unser Expertenteam kann Sie bei der Suche, Auswahl und Einstellung von
-                                qualifiziertem Personal für Ihr Unternehmen in
-                                Dubai unterstützen, um den richtigen Talentpool für Ihr Unternehmen aufzubauen.</p>
+                        <h5 class="mb-3">Подбор персонала</h5>
+                            <p class="mb-0">Наша команда экспертов может помочь вам в поиске, выборе и найме квалифицированного персонала для вашей компании в Дубае, чтобы создать правильный резерв талантов для вашей компании.</p>
                     </div>
                     <div class="service-btn rounded-0 rounded-bottom">
-                        <a class="text-primary fw-medium" href="/recruiting.php">Read More<i
-                                class="bi bi-chevron-double-right ms-2"></i></a>
+                        <a class="text-primary fw-medium" href="/recruiting.php">Подробнее<i                                class="bi bi-chevron-double-right ms-2"></i></a>
                     </div>
                 </div>
             </div>
@@ -113,16 +104,13 @@
                 <div class="service-item position-relative h-100 d-flex flex-column">
                     <div class="service-text rounded p-5">
                         <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
-                            <img class="img-fluid" src="img/icon/icon-10.png" alt="Icon">
+                            <img class="img-fluid" src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/icon/icon-10.png" alt="Icon">
                         </div>
-                        <h5 class="mb-3">Umzugsservice</h4>
-                            <p class="mb-0">Wir unterstützen Sie bei der Organisation Ihres Umzugs nach Dubai,
-                                einschließlich der Suche nach geeigneter Unterkunft,
-                                der Organisation von Transport und Logistik sowie der Anmeldung bei lokalen Behörden und
-                                Einrichtungen.</p>
+                        <h5 class="mb-3">Услуги по переезду</h5>
+                            <p class="mb-0">Мы поможем вам организовать ваш переезд в Дубай, включая поиск подходящего жилья, организацию транспорта и логистики, а также регистрацию в местных органах власти и учреждениях.</p>
                     </div>
                     <div class="service-btn rounded-0 rounded-bottom">
-                        <a class="text-primary fw-medium" href="/logistik.php">Read More<i
+                        <a class="text-primary fw-medium" href="/logistik.php">Подробнее<i
                                 class="bi bi-chevron-double-right ms-2"></i></a>
                     </div>
                 </div>
--- ru/immobilienkauf.php
+++ ru/immobilienkauf.php
@@ -6,7 +6,7 @@
 
     <!-- Carousel Start -->
     <div class="fullscreen position-relative">
-        <img src="img/kontoeroeffnung.jpg" alt="Dubai"
+        <img src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/img/kontoeroeffnung.jpg" alt="Dubai"
             style="float:left;width:100vw;height:70vh;object-fit:cover;   object-position: center">
     </div>
     <!-- Carousel End -->
@@ -17,33 +17,18 @@
         <div class="col-lg-10">
             <div class="px-5 py-5">
                 <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                    <h1 class="display-5 mb-5">Immobilienkauf VAE</h1>
+                    <h1 class="display-5 mb-5">Покупка недвижимости в ОАЭ</h1>
                 </div>
 
                 <div class="col-md-8 mx-auto">
-                <p>Der Kauf von Immobilien in den Vereinigten Arabischen Emiraten ist eine beliebte Investitionsmöglichkeit für Unternehmen
-                und Privatpersonen. Das Land bietet eine Vielzahl von Immobilienoptionen, von Apartments und Villen bis hin zu
-                gewerblichen Immobilien wie Büros und Geschäftsräumen. Der Immobilienmarkt in den VAE ist jedoch dynamisch und es ist
-                wichtig, dass Sie über fundierte Kenntnisse verfügen, bevor Sie eine Investitionsentscheidung treffen.</p>
+                    <p>Покупка недвижимости в Объединенных Арабских Эмиратах - популярная инвестиционная возможность для компаний и частных лиц. Страна предлагает широкий выбор вариантов недвижимости, от квартир и вилл до коммерческой недвижимости, такой как офисы и торговые помещения. Однако рынок недвижимости в ОАЭ динамичен, и важно иметь глубокие знания, прежде чем принимать инвестиционные решения.</p>
+                    <p>Pantheon Elite - это опытная консультационная компания, которая предлагает своим клиентам всестороннюю консультационную поддержку и помощь при покупке недвижимости в ОАЭ. Мы тесно сотрудничаем с нашими клиентами, чтобы понять их специфические требования и потребности, и определить наилучшие варианты, которые соответствуют их требованиям.</p>
+                    <p>Наш сервис по покупке недвижимости включает всестороннюю поддержку при поиске и выборе недвижимости, переговорах о ценах и условиях, а также помощь при проведении юридической проверки и завершении сделок. У нас есть обширная сеть агентов по недвижимости, которая позволяет нам быстро и эффективно идентифицировать недвижимость, которая отвечает требованиям наших клиентов.</p>
+                    <p>Мы также помогаем нашим клиентам с подачей заявок на ипотеку и финансирование для покупки недвижимости. Мы сотрудничаем с множеством финансовых институтов, чтобы обеспечить нашим клиентам доступ к лучшим вариантам ипотечного кредитования и финансирования, которые адаптированы под их потребности и требования.</p>
+                    <p>Наш сервис по покупке недвижимости также включает поддержку в соблюдении местного законодательства и правил. Мы гарантируем, что наши клиенты осведомлены обо всех юридических требованиях и ограничениях, которые действуют при покупке недвижимости в ОАЭ. Мы также помогаем нашим клиентам с подачей заявок на необходимые разрешения и лицензии, чтобы убедиться, что все требования выполняются, и покупка проходит без проблем.</p>
+                    </div>
 
-                <p>Pantheon Elite ist ein erfahrenes Beratungsunternehmen, das seinen Kunden eine umfassende Beratung und Unterstützung
-                beim Kauf von Immobilien in den VAE bietet. Wir arbeiten eng mit unseren Kunden zusammen, um deren spezifischen
-                Anforderungen und Bedürfnisse zu verstehen und die besten Optionen zu identifizieren, die ihren Anforderungen
-                entsprechen.</p>
 
-                <p>Unser Immobilienkauf-Service umfasst eine umfassende Unterstützung bei der Suche und Auswahl von Immobilien, Verhandlung
-                von Preisen und Konditionen sowie Unterstützung bei der Durchführung von rechtlichen Prüfungen und der Abwicklung von
-                Transaktionen. Wir haben ein umfangreiches Netzwerk von Immobilienmaklern, die uns ermöglichen, schnell und effektiv
-                Immobilien zu identifizieren, die den Anforderungen unserer Kunden entsprechen.</p>
-
-                <p>Wir unterstützen unsere Kunden auch bei der Beantragung von Hypotheken und Finanzierungen für den Kauf von Immobilien.
-                Wir arbeiten mit einer Vielzahl von Finanzinstituten zusammen, um unseren Kunden den Zugang zu den besten Hypotheken-
-                und Finanzierungsoptionen zu ermöglichen, die auf ihre Bedürfnisse und Anforderungen zugeschnitten sind.</p>
-
-                <p>Unser Immobilienkauf-Service umfasst auch die Unterstützung bei der Einhaltung der lokalen Gesetze und Vorschriften. Wir
-                stellen sicher, dass unsere Kunden über alle rechtlichen Anforderungen und Einschränkungen informiert sind, die beim
-                Kauf von Immobilien in den VAE gelten. Wir unterstützen unsere Kunden auch bei der Beantragung von erforderlichen
-                Genehmigungen und Lizenzen, um sicherzustellen, dass alle Anforderungen erfüllt sind und der Kauf reibungslos abläuft.</p>
                 </div>
                 </div>
         </div>
