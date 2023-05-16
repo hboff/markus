260cc005baba0548699e8df5cdaf091157fb5e7a
hboff <oskar@baucampus.de>
2023-05-16 16:15:41 +0200

    1

--- index.php
+++ index.php
@@ -408,82 +408,6 @@
     <!-- Project End -->
 
     <!-- Team Start -->
-<div class="container-xxl py-5">
-    <div class="container">
-        <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <p class="fs-5 fw-medium text-primary">Unser Team</p>
-            <h1 class="display-5 mb-5">Unsere Experten - Ihre Visionen</h1>
-        </div>
-        <div class="row g-4 team-row">
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="team-item rounded overflow-hidden pb-4">
-                    <img class="img-fluid mb-4" src="img/markus.jpeg" alt="" style="width: 100%; object-fit: cover;">
-                    <div class="team-content">
-                        <h5>Markus Maier</h5>
-                        <p class="text-primary">Founder & CEO</p>
-                        <p  style="text-align: left;"><b>Fachgebiet:</b> internationale Bankenbeziehung</p>
-                        <p  style="text-align: left;"><b>Sprachen:</b> Russisch, Deutsch, Englisch</p>
-                    </div>
-                    <ul class="team-social">
-                        <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
-                        <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="team-item rounded overflow-hidden pb-4">
-                        <img class="img-fluid mb-4" src="img/andreas.jpeg" alt="" style="width: 100%; height: 400px; object-fit: cover;">
-                    <div class="team-content">
-                        <h5>Andreas Schlegel</h5>
-                        <p class="text-primary">Founder</p>
-                        <p  style="text-align: left;"><b>Fachgebiet:</b> Immobilien</p>
-                        <p  style="text-align: left;"><b>Sprachen:</b> Russisch, Deutsch</p>
-                    </div>
-                    <ul class="team-social">
-                        <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
-                        <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="team-item rounded overflow-hidden pb-4">
-                    <img class="img-fluid mb-4" src="img/dimitri.jpeg" alt="" style="width: 100%; object-fit: cover;">
-                    <div class="team-content">
-                        <h5>Dr. Dmitrij Balliet</h5>
-                        <p class="text-primary">Partner</p>
-                        <p  style="text-align: left;"><b>Fachgebiet:</b> internationales Steuerrecht</p>
-                        <p  style="text-align: left;"><b>Sprachen:</b> Russisch, Deutsch, Englisch</p>
-                    </div>
-                    <ul class="team-social">
-                        <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
-                        <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="team-item rounded overflow-hidden pb-4">
-                    <img class="img-fluid mb-4" src="img/mathias.jpeg" alt="" style="width: 100%; object-fit: cover;">
-                    <div class="team-content">
-                        <h5>Mathias Krüger</h5>
-                        <p class="text-primary">Partner</p>
-                        <p  style="text-align: left;"><b>Fachgebiet:</b>Immobilien</p>
-                        <p  style="text-align: left;"><b>Sprachen:</b>Deutsch, Englisch</p>
-                    </div>
-                    <ul class="team-social">
-                        <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
-                        <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
-                    </ul>
-                </div>
-            </div>                        
-            <!-- Die übrigen team-items -->
-        </div>
-    </div>
-</div>
-
-
-
-
-
 
     <div class="container-xxl py-5">
         <div class="container">
@@ -498,8 +422,8 @@
                         <div class="team-content">
                             <h5>Markus Maier</h5>
                             <p class="text-primary">Founder & CEO</p>
-                            <p><b>Fachgebiet:</b> internationale Bankenbeziehung</p>
-                            <p><b>Sprachen:</b> Russisch, Deutsch, Englisch</p>
+                            <p style="text-align: left;"><b>Fachgebiet:</b> internationale Bankenbeziehung</p>
+                            <p style="text-align: left;"><b>Sprachen:</b> Russisch, Deutsch, Englisch</p>
                             <ul class="team-social">
                                 <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
                                 <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
@@ -513,8 +437,8 @@
                         <div class="team-content">                        
                         <h5>Andreas Schlegel</h5>
                         <p class="text-primary">Founder</p>
-                        <p><b>Fachgebiet:</b> Immobilien</p>
-                        <p><b>Sprachen:</b> Russisch, Deutsch</p>
+                        <p style="text-align: left;"><b>Fachgebiet:</b> Immobilien</p>
+                        <p style="text-align: left;"><b>Sprachen:</b> Russisch, Deutsch</p>
                         <ul class="team-social">
                             <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
                             <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
@@ -528,8 +452,8 @@
                         <div class="team-content">                        
                         <h5>Dr. Dmitrij Balliet</h5>
                         <p class="text-primary">Partner</p>
-                        <p><b>Fachgebiet:</b> Immobilien</p>
-                        <p><b>Sprachen:</b> Russisch, Deutsch, Englisch</p>
+                        <p style="text-align: left;"><b>Fachgebiet:</b> internationales Steuerrecht</p>
+                        <p style="text-align: left;"><b>Sprachen:</b> Russisch, Deutsch, Englisch</p>
                         <ul class="team-social">
                             <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
                             <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
@@ -543,8 +467,8 @@
                         <div class="team-content">                        
                         <h5>Mathias Krüger</h5>
                         <p class="text-primary">Partner</p>
-                        <p><b>Fachgebiet:</b>Immobilien</p>
-                        <p><b>Sprachen:</b>Deutsch, Englisch</p>
+                        <p style="text-align: left;"><b>Fachgebiet:</b>Immobilien</p>
+                        <p style="text-align: left;"><b>Sprachen:</b>Deutsch, Englisch</p>
                         <ul class="team-social">
                             <li><a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a></li>
                             <li><a class="btn btn-square" href=""><i class="fab fa-linkedin-in"></i></a></li>
