9455e64b3cedac16f79e9598ba289554f88533b6
hboff <oskar@baucampus.de>
2023-05-17 09:56:28 +0200

    full div

--- ru/index.php
+++ ru/index.php
@@ -327,6 +327,7 @@
 <section class="page-section bg-light">
     <div class="container-xxl pt-5">
         <div class="container">
+            <div class="text-center text-md-start pb-5 pb-md-0 wow fadeInUp" data-wow-delay="0.1s"
                 style="max-width: 500px;">
                 <p class="fs-5 fw-medium text-primary">Наши проекты</p>
                 <h1 class="display-5 mb-5">Некоторые из наших завершенных проектов</h1>
