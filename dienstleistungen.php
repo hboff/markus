34e528643a005fd7d916a2a97dfd7d544fccd487
hboff <oskar@baucampus.de>
2023-05-17 11:59:30 +0200

    2

--- dienstleistungen.php
+++ dienstleistungen.php
@@ -21,8 +21,7 @@
 <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <h1 class="display-5 mb-5">Ihr Partner in Dubai</h1>
-            <p class="fs-5 fw-medium text-primary">Unsere Dienstleistungen</p>
+            <h1 class="display-5 mb-5 py-5">Unsere Dienstleistungen</h1>
 
         </div>
         <div class="row g-4">
