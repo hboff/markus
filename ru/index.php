18d9448c6ba1cf84a538e35bb77e5a46c96719b2
hboff <oskar@baucampus.de>
2023-05-17 09:54:24 +0200

    3

--- ru/index.php
+++ ru/index.php
@@ -327,6 +327,7 @@
 <section class="page-section bg-light">
     <div class="container-xxl pt-5">
         <div class="container">
+                style="max-width: 500px;">
                 <p class="fs-5 fw-medium text-primary">Наши проекты</p>
                 <h1 class="display-5 mb-5">Некоторые из наших завершенных проектов</h1>
             </div>
@@ -439,11 +440,11 @@
     <!-- JavaScript Libraries -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/lib/wow/wow.min.js"></script>
+    <script src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/lib/easing/easing.min.js"></script>
+    <script src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/lib/waypoints/waypoints.min.js"></script>
+    <script src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/lib/lightbox/js/lightbox.min.js"></script>
 
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
