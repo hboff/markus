8b3dcd7abb679ff7e1d637d1d9bba8af0c888c2c
hboff <oskar@baucampus.de>
2023-05-17 12:36:55 +0200

    1

--- team.php
+++ team.php
@@ -14,7 +14,7 @@
     <!-- Team Start -->
 <div class="container-fluid">
     <div class="row justify-content-center">
-                <div class="col-lg-5">
+                <div class="col-lg-10">
             <div class="px-5 py-5">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
                 <h1 class="display-5 my-5">Unsere Experten - Ihre Visionen</h1>
