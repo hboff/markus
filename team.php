ff4f35ad10a3d3a124072d8889116014eae19cca
hboff <oskar@baucampus.de>
2023-05-17 10:53:31 +0200

    4

--- team.php
+++ team.php
@@ -15,7 +15,6 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <p class="fs-5 fw-medium text-primary">Unser Team</p>
                 <h1 class="display-5 mb-5">Unsere Experten - Ihre Visionen</h1>
             </div>
             <div class="row g-4">
