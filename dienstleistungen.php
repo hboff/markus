354b3ee2379d54bc6246866b2c8e4a231a9e0682
hboff <oskar@baucampus.de>
2023-05-17 12:05:18 +0200

    3

--- dienstleistungen.php
+++ dienstleistungen.php
@@ -21,7 +21,7 @@
 <div class="container-xxl py-5">
     <div class="container py-5">
         <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <h1 class="display-5 mb-5">Unsere Dienstleistungen</h1>
+            <h1 class="display-5 mb-5 py-5">Unsere Dienstleistungen</h1>
         </div>
         <div class="row g-4">
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
