5bb32cd9dc88e96fa4106c808578e6034d78adcf
hboff <oskar@baucampus.de>
2023-05-18 13:43:01 +0200

    90

--- header.php
+++ header.php
@@ -36,7 +36,17 @@
 
     <!-- Template Stylesheet -->
     <link href="<?php echo 'https://' . $_SERVER['HTTP_HOST'];?>/css/style.css" rel="stylesheet">
+<script>
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
+    window.onload = function() {
+        if(iOS){
+            document.querySelector('.navbar').style.backgroundColor = 'rgba(0, 255, 0, .1)'; // Zum Beispiel, grün für iOS
+        }
+        else{
+            document.querySelector('.navbar').style.backgroundColor = 'rgba(255, 255, 255, .15)'; // Ihre aktuelle Farbe
+        }
+    }
     <style>
 
         .custom-row {
