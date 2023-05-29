f2953b1c0fd8d674f59b12aaad356e867b1b02f7
hboff <oskar@baucampus.de>
2023-05-29 18:18:17 +0200

    3

--- ru/index.php
+++ ru/index.php
@@ -377,7 +377,6 @@
 <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <p class="fs-5 fw-medium text-primary">Наша Команда</p>
             <h1 class="display-5 mb-5">Наши эксперты</h1>
         </div>
         <div class="row g-4">
