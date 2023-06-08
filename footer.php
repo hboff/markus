a45dd53d4a603d14320ab73cbd494b9bd1721fb9
hboff <oskar@baucampus.de>
2023-06-08 16:49:16 +0200

    footer

--- footer.php
+++ footer.php
@@ -16,7 +16,6 @@
                     <h4 class="text-white mb-4">Links</h4>
                     <a class="btn btn-link no-hover" href="contact.php">Kontakt</a>
                     <a class="btn btn-link no-hover" href="dienstleistungen.php">Unsere Dienstleistungen</a>
-                    <a class="btn btn-link no-hover" href="datenschutzerklaerung.php">Datenschutzerklärung</a>
                     <a class="btn btn-link no-hover" href="impressum.php">Impressum</a>
                 </div>
                 <div class="col-lg-3 col-md-6">
