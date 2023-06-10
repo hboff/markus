5394c075d993fad9eddc1aa990f64474d5aa6e8b
hboff <oskar@baucampus.de>
2023-06-10 16:44:38 +0200

    dienstleistungen

---
+++ kontakt-incl-en.php
@@ -0,0 +1,17 @@
+    <!-- Kontakt2 Start -->
+    
+    <section class="page-section bg-light">
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                    <h1 class="display-5 mb-5">Contact</h1>
+                </div>
+            </div>
+            <div class="calendly-inline-widget"
+                data-url="https://calendly.com/oskarkelohse/erstbesprechung-immobilie-dubai?hide_gdpr_banner=1"
+                style="min-width:320px;height:630px;"></div>
+            <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async=""></script>
+        </div>
+    
+    </section>
+    <!-- Kontakt2 End -->
